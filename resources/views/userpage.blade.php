5553f51d96a62256b8de3796de8ffb66540d4c93
OlgaStude <indimindy@yandex.ru>
2023-04-19 20:16:24 +0400

    rnn

--- resources/views/userpage.blade.php
+++ resources/views/userpage.blade.php
@@ -43,8 +43,23 @@
         @foreach($videos as $video)
         <div class="video">
             <video src="{{ asset('storage/videos/'.$video->path) }}"></video>
+            @if($video->category == 'TVseries')
             <div class="video-category">
-            <p class="category-name">{{ $video->category }}</p>
+
+            <p class="category-name">Сериалы</p>
+            @elseif($video->category == 'cartoon')
+            <br>
+            <div class="video-category">
+            <p class="category-name">Анимация</p>
+            @elseif($video->category == 'film')
+            <br>
+            <div class="video-category">
+            <p class="category-name">Фильмы</p>
+            @elseif($video->category == 'game')
+            <br>
+            <div class="video-category">
+            <p class="category-name">Игры</p>
+            @endif
             </div>
             <p class="title-name"><a href="{{ url('video/'.$video->id) }}">{{ $video->title }}</a></p>
             <p class="date">{{ \Carbon\Carbon::parse($video->created_at)->format('d.m.Y H:m') }}</p>
@@ -56,7 +71,7 @@
             </div>
             <div class="dislike-panel">
                 <img src="{{ asset('storage/img/dislike.png') }}">
-                <p class="likes">{{ $video->likes }}</p>
+                <p class="likes">{{ $video->dislikes }}</p>
             </div>
             </div>
             <div class="restrictions">
